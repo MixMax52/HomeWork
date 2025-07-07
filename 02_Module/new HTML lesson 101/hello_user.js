b34e8b45b515f7b20273c1b7415dd5ced9bfe8b1
MixMax <mahovme@gmail.com>
2025-07-07 19:51:11 +0300

    добавил текст задания в решение

--- 02_Module/new HTML lesson 101/hello_user.js
+++ 02_Module/new HTML lesson 101/hello_user.js
@@ -1,3 +1,5 @@
+//1) Создайте функцию, которая в качестве аргумента будет принимать имя, а возвращать строчку ‘Hello “аргумент функции”’.
+
 function helloUser(firstName) {
     return console.log(`"Hello ${firstName}"`)
 }
